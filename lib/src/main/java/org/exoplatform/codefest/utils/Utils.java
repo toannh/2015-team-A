fc380c1f34d7f68a65f15d8b8a7e3d2f2b389d33
toannh <toannh@exoplatform.com>
2015-07-08 16:37:16 +0700

    Init Utils

---
+++ lib/src/main/java/org/exoplatform/codefest/utils/Utils.java
@@ -0,0 +1,27 @@
+package org.exoplatform.codefest.utils;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * Created by The eXo Platform SEA
+ * Author : eXoPlatform
+ * toannh@exoplatform.com
+ * On 7/8/15
+ * #comments here
+ */
+public class Utils {
+  private static final SimpleDateFormat formatDateTime = new SimpleDateFormat();
+  static {
+    formatDateTime.applyPattern("EEE dd/MM HH:mm a");
+  }
+
+  /**
+   * Convert friendly date time format
+   * @param time
+   * @return String datetime format
+   */
+  public static String toDate(long time){
+    return formatDateTime.format(time);
+  }
+
+}
